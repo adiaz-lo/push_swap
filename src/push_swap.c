ee9f525023e7eebc873d0c7f9cec457d0d0e43b9
Takuya <adiaz.lo@disroot.org>
2024-04-04 16:57:35 +0200

    Advanced in the parsing + moved main.c to test folder

--- src/push_swap.c
+++ src/push_swap.c
@@ -12,6 +12,77 @@
 
 #include "push_swap.h"
 
+int		ft_choose_algorithm(t_stack **lst_a, t_stack **lst_b)
+{
+	if (stack_length(*lst_a) == 2)
+		return (ft_sa(lst_a, "sa\n"), 0);
+	else if (stack_length(*lst_a) == 3)
+		sort_three_nums(lst_a);
+	else if (stack_length(*lst_a) == 4)
+		sort_four_nums(lst_a, lst_b);
+	else if (stack_length(*lst_a) == 5)
+		sort_five_nums(lst_a, lst_b);
+	else
+		ft_radix(lst_a, lst_b);
+	return (0);
+}
+
+int		are_args_sign_nums_or_spaces(int argc, char **argv)
+{
+	int		i;
+	int		j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = 0;
+		while ((argv[i][j]) && (ft_isdigit(argv[i][j])
+				|| (argv[i][j] == ' ')
+				|| (argv[i][j] == '-' && ft_isdigit(argv[i][j + 1])
+				&& !ft_isdigit(argv[i][j - 1]))
+				|| (argv[i][j] == '+' && ft_isdigit(argv[i][j + 1])
+				&& !ft_isdigit(argv[i][j - 1]))))
+			j++;
+		if (argv[i][j] && !ft_isdigit(argv[i][j]))
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+t_stack	*conv_args_to_int_and_add(int argc, char **argv)
+{
+	t_stack 	aux_lst;
+	char		**split_result;
+	int			i;
+	int			j;
+	long long	num;
+	
+	aux_lst = NULL;
+	i = 1;
+	while (i < argc)
+	{
+		
+		i++;
+	}
+
+	return (aux_lst);
+}
+
+int		is_number_repeated(t_stack *start)
+{
+	t_stack *stck_i;
+	t_stack *stck_j;
+	int		i;
+
+	stck_i = start;
+	while (i != NULL)
+	{
+		
+	}
+	return (0);
+}
+
 void	main(int argc, char **argv)
 {
 	t_stack	*lst_a;
@@ -32,7 +103,7 @@ void	main(int argc, char **argv)
 		if (are_nums_ordered(lst_a) != 0)
 			return (free_stack(&lst_a), 0);
 		if (check_repeated_numbers(lst_a) == 0 && check_numbers_in_order(lst_a) == 0)
-			ft_choose_algorithm(lst_a);
+			ft_choose_algorithm(&lst_a, &lst_b);
 		else
 			ft_error(1);
 	}
