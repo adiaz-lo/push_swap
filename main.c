b643b03c30e41af1e30f2fa4f6a2778a8543e21f
Alberto Díaz López <120560241+adiaz-lo@users.noreply.github.com>
2024-04-22 09:12:50 +0200

    Added main for backup
    Backup main from the campus iMac

---
+++ main.c
@@ -0,0 +1,52 @@
+#include "push_swap.h"
+#include "Libft/libft.h"
+
+void	show_leaks(void)
+{
+	system("leaks -q a.out");
+}
+char	**ft_strjoinArgs(int argc, char **argv)
+{
+	char	**result;
+	char	*str1 = malloc(10);
+	ft_strlcpy(str1, "Porro", 6);
+	printf("%p\n", str1);
+	free (str1);
+	char	*str = malloc(ft_strlen(argv[1]) + 1);
+	printf("%p\n", str);
+	char	*aux;
+
+	ft_strlcpy(str, argv[1], ft_strlen(argv[1]) + 1);
+	for (int i = 2; i < argc;)
+	{
+		/*printf("hola");*/
+		aux = ft_strjoin(str, " ");
+		free(str);
+		str = ft_strjoin(aux, argv[i++]);
+		free (aux);
+	}
+	result = ft_split(str, ' ');
+	free(str);
+	return (result);
+}
+
+int	main(int argc, char **argv)
+{
+	atexit(show_leaks);
+	if (argc > 1)
+	{
+		char **str = ft_strjoinArgs(argc, argv);
+		/*printf("%p\n", str);*/
+		for (int i = 0; str[i] != NULL; i++)
+		{
+			printf("%s\n", str[i]);
+			free(str[i]);
+		}
+		free(str);
+	}
+	else if (argc == 1)
+		return (0);
+	else
+		printf("ERROR\n");
+	return (0);
+}
